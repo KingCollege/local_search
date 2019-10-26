9c5d6ba3c1ef71c4cacd305e6f7cdbf58b950fe5
mandu shi <mandushi.18@gmail.com>
2019-10-26 13:17:17 +0100

    fix wrong files

---
+++ build.sh
@@ -0,0 +1,10 @@
+BASEDIR=$(dirname "$0")
+ORIGINALDIR=$(pwd)
+cd $BASEDIR
+rm -rf build
+mkdir build
+find "$PWD" -name "*.java" > build/sources.text
+cd build
+javac -d . -Xlint:deprecation @sources.txt
+rm sources.txt
+cd $ORIGINALDIR
